3c051939be8299579d6eca98cb56fc47282d2234
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-04-22 17:12:31 -0500

    Se termino el data_filter

--- filter_data.py
+++ filter_data.py
@@ -1,11 +1,90 @@
-from functools import reduce
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    }
+]
+
 
 def run():
-    my_list = [2,2,2,2,2]
+    all_python_devs = [worker['name'] for worker in DATA if worker['language'] == 'python']
+    all_Platzi_workers = [worker['name'] for worker in DATA if worker['organization'] == 'Platzi']
 
-    all_multiplied = reduce(lambda a,b: a * b, my_list)
+    adults = list(filter(lambda x: x['age'] > 18,DATA))
+    adults = list(map(lambda x: x['name'],adults))
 
-    print(all_multiplied)
+    old_people = list(map(lambda x: x | {"old": x['age'] > 70}, DATA))
+
+    print(all_python_devs)
+    print(all_Platzi_workers)
+    print(adults)
+    print(old_people)
 
 
 if __name__ == '__main__':
