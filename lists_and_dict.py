423d37834cb12569489ffd12cbe7778da0db0d41
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-04-22 13:43:49 -0500

    Se creo el archivo de listas y diccioanarios

---
+++ lists_and_dict.py
@@ -0,0 +1,30 @@
+def run():
+    my_list = [1, "hello", True, 4.5]
+    my_dict = {
+        'firtname': "Daniel",
+        'lastname': "Carmona"
+    }
+
+    super_list = [
+        {
+        'firtname': "Daniel",
+        'lastname': "Carmona"
+        },
+        {
+        'firtname': "Juan",
+        'lastname': "Modelo"
+        },
+        {
+        'firtname': "Susan",
+        'lastname': "Hernandez"
+        }
+    ]
+
+    super_dict = {
+        "natural_nums": [1,2,3,4,5],
+        "integer_nums": [-1, -2, 0 , 1 , 2],
+        "floating_nums": [1.1, 4,4, 6,78]
+    }
+
+    if __name__ == '__main__':
+        run()
